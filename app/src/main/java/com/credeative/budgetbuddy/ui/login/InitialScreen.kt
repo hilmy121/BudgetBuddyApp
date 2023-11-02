7174eb3eec59b87470f3cf14c410d7bd5f102164
Hilmy121 <caklunim@gmail.com>
2023-11-02 10:30:52 +0700

    navbar

---
+++ app/src/main/java/com/credeative/budgetbuddy/ui/login/InitialScreen.kt
@@ -0,0 +1,2 @@
+package com.credeative.budgetbuddy.ui.login
+
---
+++ app/src/main/java/com/credeative/budgetbuddy/ui/login/LoginScreen1.kt
@@ -0,0 +1,15 @@
+package com.credeative.budgetbuddy.ui.login
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LoginScreen(){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview(){
+    LoginScreen()
+}
