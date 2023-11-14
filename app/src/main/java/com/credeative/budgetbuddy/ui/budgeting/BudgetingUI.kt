45dd1f37674b297c3667cb2e4f268c0e625444ff
Hilmy121 <caklunim@gmail.com>
2023-11-14 18:39:38 +0700

    Wallet progress

--- app/src/main/java/com/credeative/budgetbuddy/ui/budgeting/BudgetingUI.kt
+++ app/src/main/java/com/credeative/budgetbuddy/ui/budgeting/BudgetingUI.kt
@@ -7,14 +7,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,6 +29,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.credeative.budgetbuddy.R
@@ -39,11 +43,29 @@ import com.credeative.budgetbuddy.ui.theme.linearGradientCardAmount
 
 
 @Composable
-fun BudgetingTopUI(modifier: Modifier = Modifier){
-//    Card(colors = CardDefaults.cardColors(containerColor = Color.Gray)) {
-//
-//    }
-    FundInformation()
+fun BudgetingTopUI(modifier: Modifier = Modifier, expenseSelected:Boolean = false){
+    Surface(modifier = modifier
+        .fillMaxWidth(), color = Color.White) {
+        Column (verticalArrangement = Arrangement.spacedBy(20.dp), horizontalAlignment = Alignment.CenterHorizontally){
+            Spacer(modifier = Modifier.height(20.dp))
+            Surface(modifier = Modifier
+                .fillMaxWidth(0.95f)
+                .height(40.dp), shape = Shape.small, border = BorderStroke(1.dp, color = Color.Gray)) {
+                Row (modifier = Modifier.fillMaxSize()){
+                    Card(modifier = Modifier.fillMaxHeight().weight(1f), colors = CardDefaults.cardColors(containerColor = PrimaryColorBg), shape = Shape.extraSmall) {
+
+                    }
+                    Card(modifier = Modifier.fillMaxHeight().weight(1f), colors = CardDefaults.cardColors(containerColor = Color.White), shape = Shape.extraSmall) {
+
+                    }
+                }
+            }
+            FundInformation()
+
+            FundFilter()
+        }
+    }
+
 }
 @Composable
 fun FundInformation(modifier:Modifier = Modifier){
@@ -68,11 +90,28 @@ fun FundInformation(modifier:Modifier = Modifier){
 }
 @Composable
 fun FundFilter(modifier:Modifier = Modifier){
-    Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly){
-        Card(modifier = Modifier.size(85.dp)) {
 
+        Row (modifier = modifier.fillMaxWidth(0.95f), horizontalArrangement = Arrangement.SpaceBetween){
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Card(modifier = Modifier.size(height = 35.dp, width = 85.dp),colors = CardDefaults.cardColors(containerColor = Color.Transparent), border = BorderStroke(1.dp,Color.Gray), shape = Shape.extraSmall) {
+
+                }
+                Text(text = "Tambah",modifier=Modifier.width(75.dp), textAlign = TextAlign.Center, maxLines = 2, style = MaterialTheme.typography.bodySmall)
+            }
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Card(modifier = Modifier.size(height = 35.dp, width = 85.dp),colors = CardDefaults.cardColors(containerColor = Color.Transparent), border = BorderStroke(1.dp,Color.Gray), shape = Shape.extraSmall) {
+
+                }
+                Text(text = "Kategori",modifier=Modifier.width(75.dp), textAlign = TextAlign.Center, maxLines = 2, style = MaterialTheme.typography.bodySmall)
+            }
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Card(modifier = Modifier.size(height = 35.dp, width = 85.dp),colors = CardDefaults.cardColors(containerColor = Color.Transparent), border = BorderStroke(1.dp,Color.Gray), shape = Shape.extraSmall) {
+
+                }
+                Text(text = "Analisa",modifier=Modifier.width(75.dp), textAlign = TextAlign.Center, maxLines = 2, style = MaterialTheme.typography.bodySmall)
+            }
         }
-    }
+
 }
 
 @Preview(showBackground = true)
