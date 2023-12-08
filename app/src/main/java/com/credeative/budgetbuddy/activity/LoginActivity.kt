682cb312a081cd68fe74f1f2754bb7f8c7834e98
Hilmy121 <caklunim@gmail.com>
2023-12-08 10:28:02 +0700

    viewpager

--- app/src/main/java/com/credeative/budgetbuddy/activity/LoginActivity.kt
+++ app/src/main/java/com/credeative/budgetbuddy/activity/LoginActivity.kt
@@ -6,13 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.credeative.budgetbuddy.ui.login.LoginScreen
-import com.credeative.budgetbuddy.ui.login.ViewpagerApp
-import com.credeative.budgetbuddy.ui.login.ViewpagerAppExample
 import com.credeative.budgetbuddy.ui.theme.BudgetBuddyTheme
 
 class LoginActivity : ComponentActivity() {
